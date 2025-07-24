786e1f4e2cc59b9fb5ff6639cd04960fad68a0d0
Rithika <rithikarithuzz499@gmail.com>
2025-07-24 15:27:35 +0530

    Add files via upload

---
+++ ATM.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int choice;
+float balance=1000.00,amount;
+while(1){
+    printf("1.balance\n");
+    printf("2.deposit money\n");
+    printf("3.withdraw money\n");
+    printf("4.exit\n");
+    printf("entr a choice:");}
+}
